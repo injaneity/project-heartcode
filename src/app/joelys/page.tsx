a0a4baf08a27063cea9e6e0500072bf481626cdb
JoelysWee <joelysweekerrin@gmail.com>
2024-12-11 10:28:18 +0800

    vaniapage

--- src/app/joelys/page.tsx
+++ src/app/joelys/page.tsx
@@ -1,11 +1,14 @@
 export default function MenteePage() {
 
     return (
-        <div style={{ backgroundImage: `url(/assets/idontknow.jpg)`, backgroundRepeat: "no-repeat", backgroundSize: "contain",
+        <div
+            style={{ backgroundImage: `url(/assets/idontknow.jpg)`, backgroundRepeat: "no-repeat", backgroundSize: "contain",
             width: "100%",
             height: "1080px"
          }}>
-            Hello joelys
+            <div className="flex flex-col justify-center items-center text-4xl p-10">
+                <b>Hello joelys</b>
+            </div>
         </div>
     );
 }
