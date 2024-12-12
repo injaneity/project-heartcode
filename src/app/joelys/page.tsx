f9a49e38431ac540329723fec351bd2831e597b7
JoelysWee <joelysweekerrin@gmail.com>
2024-12-12 15:47:46 +0800

    joelysaboutme

--- src/app/joelys/page.tsx
+++ src/app/joelys/page.tsx
@@ -1,14 +1,28 @@
 export default function MenteePage() {
-
     return (
         <div
-            style={{ backgroundImage: `url(/assets/idontknow.jpg)`, backgroundRepeat: "no-repeat", backgroundSize: "contain",
-            width: "100%",
-            height: "1080px"
-         }}>
-            <div className="flex flex-col justify-center items-center text-4xl p-10">
-                <b>Hello joelys</b>
-            </div>
+            className="relative text-black"
+            style={{
+                backgroundImage: `url(/assets/idontknow.jpg)`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                width: "100%",
+                height: "1080px"
+            }}
+        >
+            {/* Background overlay */}
+            <div
+                className="absolute inset-0 bg-black opacity-50"
+                style={{
+                    zIndex: -1,
+                }}
+            ></div>
+
+            {/* Text content */}
+            <span className="flex flex-col justify-center text-4xl p-10 w-2/3 gap-4">
+                <b>Hi, my name is Joelys</b>
+                <p>I am part of my school girl guides contingent and completed an Anti-Drug Abuse course.</p>
+            </span>
         </div>
     );
 }
