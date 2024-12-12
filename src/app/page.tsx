970a05130984cb68fb366f3ffd40e5f95e73c0f5
Zane Chee <zane.chee.2023@scis.smu.edu.sg>
2024-12-12 10:35:52 +0800

    feat:brainrot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
   Card,
   CardDescription,
@@ -5,10 +7,24 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
+import ReactPlayer from "react-player";
+
 
 export default function Home() {
   return(
     <div>
+      <div className="flex flex-row justify-center">
+        <ReactPlayer 
+          url={'https://www.youtube.com/watch?v=n_Dv4JMiwK8?mute=1'}
+          playing={true}
+        />
+    
+        <ReactPlayer 
+          url={'https://www.youtube.com/watch?v=eRXE8Aebp7s&mute=1'}
+          playing={true}
+        />
+      </div>
+      
       <Card>
   <CardHeader>
     <CardTitle>Hi, im joelys</CardTitle>
